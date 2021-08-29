8d1253a5e5b98696c9b4fda82145cbfb026a0b0b
kanishkaditya <shuklakanishkaditya12@gmail.com>
2021-08-29 13:13:27 +0530

    Fixed bug of page not loading

--- JavaScripts/About.js
+++ JavaScripts/About.js
@@ -330,7 +330,7 @@ THE SOFTWARE.
   const button = document.querySelector(".title-hover");
   
   function pageRedirect() {
-    window.location.replace("../Home.html");
+    window.location.replace("./Home.html");
   }
   
   button.addEventListener("click", () => {
